a51def46784fbbc156d8a40853086dec6f494417
Aquafortis <ian@320up.com.au>
2017-09-16 18:39:05 +1000

    First Install

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+// Copy and paste into your own theme if required
+function wooalign_enqueue_scripts() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+  // Enqueue the wooalign-public.js for Woo Align Buttons
+  wp_enqueue_script( 'woo-align-buttons', get_stylesheet_directory_uri() . '/js/wooalign-public.js', array( 'jquery' ), '3.1.1', true );
+}
+add_action( 'wp_enqueue_scripts', 'wooalign_enqueue_scripts' );
+
+// Add wrapper for Woo Align Buttons
+add_action( 'woocommerce_before_shop_loop_item', 'wooalign_product_link_open', 8 );
+function wooalign_product_link_open() {
+  echo '<div id="woo-height">';
+}
+
+add_action( 'woocommerce_after_shop_loop_item', 'wooalign_product_link_close', 4 );
+function wooalign_product_link_close() {
+  echo '</div>';
+}
